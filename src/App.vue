6388ac001240d46e089a04a50f42bd622ec08fee
Yiğit Tosun <yigit-tosun@outlook.com>
2022-07-31 18:12:32 +0300

    Create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <nav></nav>
+  <nav>
+    <div>
+      <TopBar />
+    </div>
+  </nav>
   <router-view />
 </template>
+
+<script>
+import TopBar from "@/components/Topbar-menu.vue";
+export default {
+  components: {
+    TopBar,
+  },
+};
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/Topbar-menu.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="bg-gradient-to-r from-cyan-600 to-red-600">
+    <nav
+      class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center"
+    >
+      <div class="flex items-center justify-between">
+        <h1 class="items-center md:text-xl font-bold text-gray-100">
+          {{ $route.name }}
+        </h1>
+        <!-- Mobile menu button -->
+        <div @click="toggleNav" class="flex md:hidden">
+          <button
+            type="button"
+            class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+          >
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+              <path
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+      <div
+        :class="showMenu ? 'flex' : 'hidden'"
+        class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
+      >
+        <router-link
+          to="/"
+          class="text-xl text-gray-100 md:text-lg hover:text-slate-300"
+          >Home
+        </router-link>
+        <router-link
+          to="/units"
+          class="text-xl text-gray-100 md:text-lg hover:text-slate-300"
+          >Units
+        </router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
